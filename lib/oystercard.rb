9f9714ed8ed5391f2457257bdcac482712a74ac2
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-08 17:28:41 +0100

    Touch in can store entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,12 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
   MAXIMUM_LIMIT = 90
   MIN_FARE = 1
   
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(value)
@@ -14,19 +14,18 @@ class Oystercard
     @balance += value
   end
 
-  def touch_in
+  def touch_in(station)
     fail 'Insufficient funds, top up card' if min_fare?
-    
-    @in_journey = true
+    @entry_station = station
   end
   
   def touch_out
     deduct(MIN_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
   
   def in_journey?
-    @in_journey
+    @entry_station
   end
 
   private 
@@ -41,5 +40,4 @@ class Oystercard
   def deduct(value)
     @balance -= value
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station) { double :station }
+
   it 'allows the user to create an instance of Oystercard' do
     is_expected.to be_instance_of Oystercard
   end
@@ -37,27 +39,32 @@ describe Oystercard do
 
   context '#in_journey?' do
     it 'is initially not in a journey' do
-      expect(subject.in_journey?).to eq false
+      expect(subject.in_journey?).to eq nil
     end
   end 
   
   context '#touch_in' do
     it 'turns true when we touch in' do
       allow(subject).to receive(:min_fare?).and_return false
-      subject.touch_in
-      expect(subject.in_journey?).to eq true
+      subject.touch_in(station)
+      expect(subject.in_journey?).to be_truthy
     end
     it 'raises an error when someone tries to touch in without min fare on card' do
-      expect { subject.touch_in }.to raise_error 'Insufficient funds, top up card'
+      expect { subject.touch_in(station) }.to raise_error 'Insufficient funds, top up card'
+    end
+    it 'stores the value of entry station' do
+      allow(subject).to receive(:min_fare?).and_return false
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
     end
   end 
   
   context '#touch_out' do
     it 'turns when we touch out' do
       allow(subject).to receive(:min_fare?).and_return false
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
-      expect(subject.in_journey?).to eq false
+      expect(subject.in_journey?).to eq nil
     end
     it 'charges the minimum amount' do
       min_fare = Oystercard::MIN_FARE
